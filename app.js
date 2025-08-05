f38ded0a59febf901587b9391c7c6cfe03baf0b6
Iyuu8 <anesbenaziza19@gmail.com>
2025-08-05 16:07:22 +0100

    version 2 slider and nav hover added

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// for the slider
+
 const sliders = document.querySelectorAll(".hero-slider");
 const sliderInit = (slider)=>{
     const container = slider.querySelector(".slider-content");
@@ -16,8 +18,6 @@ const sliderInit = (slider)=>{
         prev.classList.remove("disabled");
     }
     next.addEventListener("click",slideNext);
-
-
     const slidePrev = ()=>{
         pose--;
         container.style.transform = `translateX(-${container.children[pose].offsetLeft}px)`;
@@ -28,5 +28,61 @@ const sliderInit = (slider)=>{
     prev.addEventListener("click",slidePrev);
     prev.classList.add("disabled");
 }
-
 sliders.forEach(val => sliderInit(val));
+
+// search bar functions and evenListeners
+const searchForm = document.querySelector(".search-form");
+const searchBar = document.querySelector(".search-bar");
+const filter = document.querySelector(".filter");
+searchBar.addEventListener("click",(e)=>{
+    e.stopPropagation();    
+    searchBar.style.backgroundColor="var(--hilight)";
+    searchForm.querySelector("#filterOn").classList.remove("disabled");
+    filter.style.color="var(--text-color)";
+    searchBar.placeholder=""; 
+    searchBar.style.paddingLeft="23%";   
+})
+const disFilter = ()=>{
+    searchForm.querySelector("#filterOn").classList.add("disabled");
+    filter.style.color="var(--hilight)";
+    searchBar.placeholder="Find Movies & TV"; 
+    searchBar.style.paddingLeft="3rem";
+    searchBar.style.backgroundColor="var(--search-bar-background)";
+}
+
+//main functionality
+
+
+
+// hover effect on nav items ( supposed to be links)
+const navItemsContainer = document.querySelector(".nav-links");
+const navItems = document.querySelectorAll(".nav-links li");
+const hoverSlider = document.querySelector(".hover-info");
+const hoverContainer = document.querySelector(".hover-content")
+navItems.forEach((item)=>{
+    item.addEventListener("mouseover",(e)=>{
+        e.stopPropagation();
+        let pos=Number(getComputedStyle(item).getPropertyValue("--pos")); 
+        hoverSlider.style.display="flex"; 
+        hoverSlider.style.transform=`translateX(${item.offsetLeft-hoverSlider.offsetLeft}px) translateX(-50%)`;
+        hoverContainer.style.transform=`translateX(-${hoverContainer.children[pos].offsetLeft}px)`;
+        let height = hoverContainer.children[pos].getBoundingClientRect().height;
+        hoverContainer.style.height=`${height}px`;
+        hoverSlider.style.height=`${height}px`;
+    })
+})
+
+const disHover = ()=>{
+    hoverSlider.style.display="none";     
+}
+
+
+// to cancel effects from eventListeners...
+document.addEventListener("click",(e)=>{
+    if(!searchForm.contains(e.target)) disFilter();
+})
+document.addEventListener("mouseover",(e)=>{
+    if(!navItemsContainer.contains(e.target) && !hoverSlider.contains(e.target)){
+        disHover();
+    }
+})
